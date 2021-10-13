85c2bd4ccaf1c6aca0dd4d3fefb8e27597b85dae
Ruslan <90761009+Nechtour@users.noreply.github.com>
2021-10-13 15:45:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// $(window).on('load', function(){
+    
+// });
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  $('#header').vide('cower',{
+    bgColor: '#64947b'
+  });
+});
